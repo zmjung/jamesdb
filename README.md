16f0e9d254e0a1d5c3805b53c01bc743ef076410
zmjung <jamesjung1123@gmail.com>
2025-08-04 20:34:22 -0700

    cleaned up new lines and added config location
    Signed-off-by: zmjung <jamesjung1123@gmail.com>

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ Graph database made by James.
 Next step:
 - [ ] https://codezup.com/building-scalable-restful-apis-with-go-and-gin-step-by-step-guide/
 - [ ] file -> memory (don't hold graph in memory for now)
-- [ ] switch over to [slog](https://betterstack.com/community/guides/logging/logging-in-go/)
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"log/slog"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -13,13 +15,17 @@ import (
 )
 
 func main() {
+	configPath := flag.String("config", "config.yml", "configuration file path")
+	flag.Parse()
+
 	var cfg config.Config
-	err := config.LoadConfig(&cfg, "config.yml")
+	err := config.LoadConfig(&cfg, *configPath)
 	if err != nil {
 		panic("Failed to load configuration: " + err.Error())
 	}
 
 	log.SetDefaultLogger(cfg)
+	slog.Debug("Using config file", "configPath", *configPath, "config", cfg)
 
 	engine := gin.Default()
 	engine.Use(middleware.GetLogging())
